9a99d86cebe444d79791bcb7a8eb48acc81f38a1
Amcoogamer <althafyazin@gmail.com>
2025-05-24 18:41:15 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/callingapibackend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.callingapibackend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'postmodel.dart';
+import 'package:http/http.dart' as http;
+
+Future<List<Post>> fetchPosts() async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+  if (response.statusCode == 200) {
+    List jsonResponse = jsonDecode(response.body);
+    return jsonResponse.map((post) => Post.fromJson(post)).toList();
+  } else {
+    throw Exception('Failed to load posts');
+  }
+}
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter API Lab',
+      home: const PostList(),
+    );
+  }
+}
+
+class PostList extends StatefulWidget {
+  const PostList({super.key});
+
+  @override
+  State<PostList> createState() => _PostListState();
+}
+
+class _PostListState extends State<PostList> {
+  late Future<List<Post>> futurePosts;
+
+  @override
+  void initState() {
+    super.initState();
+    futurePosts = fetchPosts();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('API Data'),
+        ),
+        body: FutureBuilder<List<Post>>(
+            future: futurePosts,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(snapshot.data![index].title),
+                        subtitle: Text(snapshot.data![index].body),
+                      );
+                    });
+              } else {
+                return const Center(child: Text('No data available'));
+              }
+            }));
+  }
+}
